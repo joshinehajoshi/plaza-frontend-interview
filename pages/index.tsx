e2cb68a00dd9c209b69a8a8b9ba6308d7337dd55
Aditya Raj <raj@useplaza.com>
2022-08-26 17:33:56 +0530

    add input chars limit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,24 @@
 import { Controller, useForm } from "react-hook-form";
 
+interface IPostForm {
+  title: string;
+  content: string;
+  media: string;
+}
+
 const PostForm = () => {
-  const methods = useForm<any>({
-    defaultValues: {},
+  const methods = useForm<IPostForm>({
+    defaultValues: {
+      title: "",
+      content: "",
+      media: "",
+    },
   });
 
-  const { handleSubmit, control } = methods;
+  const { handleSubmit, control, watch } = methods;
+  const contentChars = watch("content");
 
-  const onSubmit = (data: any) => {
+  const onSubmit = (data: IPostForm) => {
     alert(JSON.stringify(data));
   };
 
@@ -15,9 +26,9 @@ const PostForm = () => {
     <div className="flex items-center justify-center h-screen">
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="w-96 h-3/4 rounded-md bg-white shadow-md p-4"
+        className="w-96 rounded-md bg-white shadow-md p-4"
       >
-        <div>
+        <div className="mt-4">
           <label className="block text-sm font-light text-gray-700">
             Post Title
           </label>
@@ -41,17 +52,24 @@ const PostForm = () => {
             Post Content
           </label>
           <Controller
-            name="title"
+            name="content"
             control={control}
             render={({ field }) => (
-              <div className="mt-1 appearance-none w-full border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-plazablue500 focus:border-plazablue500 text-sm focus:ring-1">
-                <textarea
-                  className="w-full flex-1 rounded-md border-0 px-3 py-2 text-sm focus:outline-none"
-                  minLength={140}
-                  maxLength={2400}
-                  {...field}
-                />
-              </div>
+              <>
+                <div className="mt-1 appearance-none w-full border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-plazablue500 focus:border-plazablue500 text-sm focus:ring-1">
+                  <textarea
+                    className="w-full flex-1 rounded-md border-0 px-3 py-2 text-sm focus:outline-none"
+                    minLength={140}
+                    maxLength={2400}
+                    {...field}
+                  />
+                </div>
+                {contentChars && (
+                  <p className="mt-1 flex justify-end text-xs text-gray-500">
+                    {2400 - contentChars.length} characters left
+                  </p>
+                )}
+              </>
             )}
           />
         </div>
@@ -66,26 +84,17 @@ const PostForm = () => {
               <div>
                 {field.value ? (
                   <div className="flex h-40 w-40 justify-center overflow-hidden rounded border bg-black">
-                    {/* <div className="relative h-full">
+                    <div className="relative h-full">
                       <img
                         className="h-full object-contain"
                         alt=""
-                        src={filePreview}
+                        src={field.value}
                       />
-                      {uploadProgress !== 100 && (
-                        <div className="absolute inset-0 flex items-center justify-center">
-                          <CircularProgressbar
-                            value={uploadProgress}
-                            className="h-20 w-20"
-                          />
-                        </div>
-                      )}
-                    </div> */}
+                    </div>
                   </div>
                 ) : (
                   <div className="h-40 w-40 rounded border">
                     <div className="flex h-full flex-col items-center justify-center">
-                      {/* <PlusIcon className="mb-2 h-8" /> */}
                       <input id="media" type="file" className="hidden" />
                       <label
                         htmlFor="media"
@@ -100,17 +109,11 @@ const PostForm = () => {
             )}
           />
         </div>
-        {/* <div className="flex justify-end pt-4">
-          <PlazaSubmitButton
-            mode="contained"
-            theme="primary"
-            size="medium"
-            btnState={getBtnState(isSubmitSuccessful, isSubmitting)}
-          >
-            <UpdateIcon />
-            <span className="ml-1">{mode === "EDIT" ? "Update" : "Add"}</span>
-          </PlazaSubmitButton>
-        </div> */}
+        <div className="mt-8">
+          <button className="flex items-center justify-center rounded-md font-medium shadow-sm focus:outline-none text-white bg-gray-800 hover:bg-black focus:ring-gray-900 focus:ring-1 focus:ring-offset-1 px-4 py-2 text-sm w-full">
+            Submit
+          </button>
+        </div>
       </form>
     </div>
   );
